c6d600a530acb9641d97bc05b7f0780e05801b62
tuiusx <tuiusx@icloud.com>
2020-05-30 10:19:18 -0300

    Ciclo de vida do componente

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -6,12 +6,30 @@ class TechList extends Component {
     /** O State e imutavel so pode ser mudado usando o setState */
     state = {
         newTech: '',
-        techs: [
-            'Node.js',
-            'ReactJS',
-            'React Native'
-        ]
+        techs: []
     };
+
+    /** Executado assim que o componente aparece em tela */
+    componentDidMount() {
+        const techs = localStorage.getItem('techs');
+
+        if (techs) {
+            this.setState({ techs: JSON.parse(techs) });
+        }
+    }
+
+    /** Executado sempre que houver alterações nas props ou estado */
+    componentDidUpdate(_, prevState) {
+        // this.props, this.state
+        if (prevState.techs !== this.state.techs) {
+            localStorage.setItem('techs', JSON.stringify(this.state.techs))
+        }
+    }
+
+    /** Executado quando o componente deixa de existir */
+    componentWillMount() {
+
+    }
 
     handleInputChange = e => {
         this.setState({ newTech: e.target.value });
